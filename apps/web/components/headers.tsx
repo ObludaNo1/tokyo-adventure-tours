feee67f6588e22db856fd73c66ca02b878d7afb7
Ondřej Caha <cahaondra@gmail.com>
2026-04-10 22:18:50 +0900

    Improved headers styles

--- apps/web/components/headers.tsx
+++ apps/web/components/headers.tsx
@@ -1,17 +1,17 @@
 import type { ReactNode } from "react";
 
 export function Header1({ children }: { children: ReactNode }) {
-  return <h1 className="text-6xl">{children}</h1>;
+  return <h1 className="text-6xl pt-18 pb-6">{children}</h1>;
 }
 
 export function Header2({ children }: { children: ReactNode }) {
-  return <h2 className="text-4xl">{children}</h2>;
+  return <h2 className="text-4xl pt-12 pb-4">{children}</h2>;
 }
 
 export function Header3({ children }: { children: ReactNode }) {
-  return <h3 className="text-2xl">{children}</h3>;
+  return <h3 className="text-2xl pt-6 pb-2">{children}</h3>;
 }
 
 export function Header4({ children }: { children: ReactNode }) {
-  return <h4 className="text-xl">{children}</h4>;
+  return <h4 className="text-xl pt-3 pb-1">{children}</h4>;
 }
